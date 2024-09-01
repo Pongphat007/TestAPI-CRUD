a541663bc15232e52639ae9e2de1d394d24994f8
GAMING PC <GAMING PC@DESKTOP-5FTJA9F>
2024-09-01 12:48:00 +0700

    Add project files.

---
+++ WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private static List<ProductList> _ProductList = new List<ProductList>();
+
+        [HttpPost]
+        [Route("api/CreateProductList")]
+        public IActionResult CreateProductList([FromBody] ProductList data)
+        {
+            data.id = _ProductList.Count + 1;
+            _ProductList.Add(data);
+
+            return Ok(_ProductList);
+        }
+
+        [HttpGet]
+        [Route("api/GetAllProductList")]
+        public IActionResult GetAllProductList()
+        {
+            return Ok(_ProductList);
+        }
+
+        [HttpPut]
+        [Route("api/UpdateProductById")]
+        public IActionResult UpdateProductById([FromBody] ProductList data)
+        {
+            try
+            {
+                if(!string.IsNullOrEmpty(data.id.ToString()))
+                {
+                    var product = _ProductList.FirstOrDefault(p => p.id == data.id);
+                    if(product != null)
+                    {
+                        product.name = data.name;  
+                        product.description = data.description;
+                        product.Price = data.Price;
+
+                        return Ok(_ProductList);
+                    }
+                    else
+                    {
+                        throw new Exception("Not found id");
+                    }
+                    
+                }
+                else
+                {
+                    throw new Exception("Not found id");
+                }
+            }
+            catch (Exception ex)
+            {
+
+               return BadRequest(ex);
+            }
+          
+        }
+    }
+}
---
+++ WebApplication1/Models/ProductList.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Models
+{
+    public class ProductList
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public decimal Price { get; set; }
+      
+    }
+}
